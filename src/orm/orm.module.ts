f295718c1593347c68f590ce8acc510699c07a72
Sn-Kinos <dony435@naver.com>
2022-09-29 12:38:13 +0900

    feat(orm): Entity 연동

--- src/orm/orm.module.ts
+++ src/orm/orm.module.ts
@@ -1,11 +1,14 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { Comment } from 'src/comment/entities/comment.entity';
+import { Keyword } from 'src/keyword/entities/keyword.entity';
+import { Post } from 'src/post/entities/post.entity';
 
 @Module({
   imports: [
     MikroOrmModule.forRoot(),
     MikroOrmModule.forFeature({
-      // entities: [Author, Book, BookTag, Publisher],
+      entities: [Post, Comment, Keyword],
     }),
   ],
   exports: [MikroOrmModule],
