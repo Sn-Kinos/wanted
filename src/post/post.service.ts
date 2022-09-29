dce3895b52dc6607f1d3b65a7aa64cb6c42af460
Sn-Kinos <dony435@naver.com>
2022-09-29 18:59:38 +0900

    feat(nest): GET /posts 임시 커밋

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,15 +1,23 @@
+import { EntityRepository } from '@mikro-orm/core';
+import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Post } from './entities/post.entity';
 
 @Injectable()
 export class PostService {
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: EntityRepository<Post>
+  ) {}
+
   create(createPostDto: CreatePostDto) {
     return 'This action adds a new post';
   }
 
   findAll() {
-    return `This action returns all post`;
+    return this.postRepository.findAll();
   }
 
   findOne(id: number) {
