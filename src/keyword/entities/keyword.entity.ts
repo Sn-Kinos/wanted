2443d407ec1edfa977fc0721481c9fd0360b1ded
Sn-Kinos <dony435@naver.com>
2022-09-29 14:04:55 +0900

    feat(orm): Keyword 엔티티에 PK 추가

--- src/keyword/entities/keyword.entity.ts
+++ src/keyword/entities/keyword.entity.ts
@@ -1,7 +1,10 @@
-import { Entity, Property } from '@mikro-orm/core';
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 
 @Entity()
 export class Keyword {
+  @PrimaryKey({ columnType: 'bigint' })
+  index!: string;
+
   @Property({ length: 255, nullable: true })
   writer?: string;
 
