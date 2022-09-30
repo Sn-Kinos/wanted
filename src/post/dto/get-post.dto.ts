3806760304eb1cdc37c04ff231498572bcdf9735
Sn-Kinos <dony435@naver.com>
2022-09-30 15:48:30 +0900

    fix(nest): GET /posts 파라미터 조건 수정

--- src/post/dto/get-post.dto.ts
+++ src/post/dto/get-post.dto.ts
@@ -1,17 +1,12 @@
 import { PartialType, PickType } from '@nestjs/mapped-types';
-import { IsNotEmpty } from 'class-validator';
 import { Post } from '../entities/post.entity';
 
 export class GetPostDto extends PartialType(PickType(Post, ['title', 'writer'])) {
-  @IsNotEmpty()
   title: string;
 
-  @IsNotEmpty()
   writer: string;
 
-  @IsNotEmpty()
   page: number;
 
-  @IsNotEmpty()
   limit: number;
 }
